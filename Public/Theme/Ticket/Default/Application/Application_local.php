733e8210b11d3b669aab5f9785ea062d6ab6fb35
luoboss <dev@pescms.com>
2019-07-26 16:03:10 +0800

    添加插件版本更新验证功能。

--- Public/Theme/Ticket/Default/Application/Application_local.php
+++ Public/Theme/Ticket/Default/Application/Application_local.php
@@ -56,6 +56,8 @@
                                 <a href="<?= $label->url(GROUP.'-Application-Init', ['n' => $value['index'], 'f' => 'option']) ?>" class="am-badge am-badge-warning am-radius">配置</a>
                                 <?php endif; ?>
 
+                                <a href="javascript:;" class="am-badge am-badge-warning am-radius ajax-click ajax-dialog check-update am-hide" msg="请确保已备份本插件，更新过程存在出错的可能" name="<?= $value['info']['name'] ?>" version="<?= $value['info']['version'] ?>">有可用更新</a>
+
                                 <a href="<?= $label->url(GROUP.'-Application-Init', ['n' => $value['index'], 'f' => 'remove']) ?>" class="am-badge am-badge-danger am-radius ajax-click ajax-dialog" msg="您确定要此删除插件吗?">删除</a>
                             </td>
                         </tr>
@@ -66,3 +68,30 @@
         </div>
     </div>
 </div>
+<script>
+    $(function(){
+        $('.check-update').each(function(){
+            var dom = $(this)
+            var name = dom.attr('name');
+            var version = dom.attr('version');
+            $.ajax({
+                url: "http://www.pc.com/?g=Api&m=Application&a=download&check=1&name="+name+"&check_version="+version,
+                type: "GET",
+                dataType:'JSON',
+                success: function (data) {
+                    if(data.status == 200){
+                        dom.removeClass('am-hide');
+                    }
+                },
+                error:function(obj){
+                    var d = dialog({
+                        title: '提示',
+                        content: '检查插件更新时出错'
+                    })
+                    d.showModal();
+                }
+            })
+
+        })
+    })
+</script>
